f3efbea1e702cb45300187b2b474767a4146ca6a
avnshsingh <avinashsingh888555@gmail.com>
2023-11-11 17:55:37 +0530

    added theme toggle and theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
-import Link from 'next/link';
 import React from 'react';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import styles from './homepage.module.css';
+import Featured from '../components/featured/Featured';
+import CategoryList from '../components/categoryList/CategoryList';
+import CardList from '../components/cardList/CardList';
+import Menu from '../components/menu/Menu';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './cardList.module.css';
+import Pagination from '../pagination/Pagination';
 
 const CardList = () => {
-  return <div>CardList</div>;
+  return (
+    <div>
+      CardList
+      <Pagination />
+    </div>
+  );
 };
 
 export default CardList;
